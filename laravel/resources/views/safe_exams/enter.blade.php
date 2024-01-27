a9eedefaec134f6ead3d4ac64f008294d56ab4f7
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-01-28 00:05:19 +0100

    Mostrar los botones en grande

--- laravel/resources/views/safe_exams/enter.blade.php
+++ laravel/resources/views/safe_exams/enter.blade.php
@@ -2,12 +2,18 @@
 
 @section('content')
 
-    @include('partials.titular', ['titular' => __('Welcome'), 'subtitulo' => ''])
+    <div class="row justify-content-center">
+        <div class="col-4">
+            @if($seb_session)
+                <a class="btn btn-success d-block py-5 mb-5"
+                   href="{{ $safe_exam->url }}">{{ __('Click here to open the application') }}</a>
+                <a class="btn btn-danger d-block"
+                   href="{{ $sebs_exit_url }}">{{ __('Click here to exit Safe Exam Browser') }}</a>
+            @else
+                <a class="btn btn-primary d-block py-5"
+                   href="{{ $sebs_url }}">{{ __('Click here to open Safe Exam Browser') }}</a>
+            @endif
+        </div>
+    </div>
 
-    @if($seb_session)
-        <a class="btn btn-success" href="{{ $safe_exam->url }}">Click here to open application</a>
-        <a class="btn btn-danger" href="{{ $sebs_exit_url }}">Click here to exit Safe Exam Browser</a>
-    @else
-        <a class="btn btn-primary" href="{{ $sebs_url }}">Click here to open Safe Exam Browser</a>
-    @endif
 @endsection
