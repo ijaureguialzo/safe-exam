ebd07b579e9c5dd94dfdac16a90a64fcd72475a5
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-01-28 23:31:06 +0100

    Mantener los datos anteriores en los formularios

--- laravel/resources/views/safe_exams/create.blade.php
+++ laravel/resources/views/safe_exams/create.blade.php
@@ -9,7 +9,8 @@
         <div class="row mb-3">
             <label class="col-2 form-label" for="classroom">{{ __('Classroom') }}</label>
             <div class="col-10">
-                <input class="form-control" type="text" id="classroom" name="classroom" placeholder="test"/>
+                <input class="form-control" type="text" id="classroom" name="classroom" placeholder="test"
+                       value="{{ old('classroom') }}"/>
                 <span class="text-danger">{{ $errors->first('classroom') }}</span>
             </div>
         </div>
@@ -17,7 +18,7 @@
             <label class="col-2 form-label" for="url">{{ __('URL') }}</label>
             <div class="col-10">
                 <input class="form-control" type="text" id="url" name="url"
-                       placeholder="https://api.socrative.comm/fake/123456"/>
+                       placeholder="https://api.socrative.comm/fake/123456" value="{{ old('url') }}"/>
                 <span class="text-danger">{{ $errors->first('url') }}</span>
             </div>
         </div>
--- laravel/resources/views/safe_exams/edit.blade.php
+++ laravel/resources/views/safe_exams/edit.blade.php
@@ -11,14 +11,14 @@
             <label class="col-2 form-label" for="classroom">{{ __('Classroom') }}</label>
             <div class="col-10">
                 <input class="form-control" type="text" id="classroom" name="classroom"
-                       value="{{ $safe_exam->classroom }}" placeholder="test"/>
+                       value="{{ old('classroom') ?: $safe_exam->classroom }}" placeholder="test"/>
                 <span class="text-danger">{{ $errors->first('classroom') }}</span>
             </div>
         </div>
         <div class="row mb-3">
             <label class="col-2 form-label" for="url">{{ __('URL') }}</label>
             <div class="col-10">
-                <input class="form-control" type="text" id="url" name="url" value="{{ $safe_exam->url }}"
+                <input class="form-control" type="text" id="url" name="url" value="{{ old('url') ?: $safe_exam->url }}"
                        placeholder="https://api.socrative.comm/fake/123456"/>
                 <span class="text-danger">{{ $errors->first('url') }}</span>
             </div>
