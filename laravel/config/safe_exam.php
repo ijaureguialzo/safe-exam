d26956e6f632f01e001aa71e90834381f65b4008
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-02-03 22:38:34 +0100

    Controlar si se permite recuperar contraseña

--- laravel/config/safe_exam.php
+++ laravel/config/safe_exam.php
@@ -4,4 +4,5 @@ return [
     'token_bytes' => env('SAFE_EXAM_TOKEN_BYTES', 8),
     'quit_password_bytes' => env('SAFE_EXAM_QUIT_PASSWORD_BYTES', 2),
     'registration_enabled' => env('SAFE_EXAM_REGISTRATION_ENABLED', false),
+    'password_reset_enabled' => env('SAFE_EXAM_PASSWORD_RESET_ENABLED', false),
 ];
