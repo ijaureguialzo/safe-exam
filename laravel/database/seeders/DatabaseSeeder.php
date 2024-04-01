ce2cc448eb3bd9ad2bd8e4edb33615c54e4ffa01
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-04-01 17:31:19 +0200

    Seeder mejorado

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -3,9 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\AllowedApp;
 use App\Models\SafeExam;
+use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,28 +15,55 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('users')->insert([
+        $user = User::create([
             'name' => 'Test',
             'email' => 'test@test.com',
-            'password' => '$2y$12$kPgMZ8H33vcLyh7ICJKkr.CReGwe28EjiYz0ayScPZ.tv3Gg.htxG',   // 12345Abcde
+            'password' => bcrypt('12345Abcde'),
             'email_verified_at' => now(),
         ]);
 
-        DB::table('safe_exams')->insert([
+        $safe_exam = SafeExam::create([
             'classroom' => 'test',
             'url' => 'https://api.socrative.comm/fake/123456',
             'token' => SafeExam::new_token(),
             'quit_password' => SafeExam::new_quit_password(),
-            'user_id' => 1,
+            'user_id' => $user->id,
         ]);
 
-        DB::table('allowed_apps')->insert([
+        AllowedApp::create([
             'title' => 'idea64',
             'executable' => 'idea64.exe',
             'path' => '%LOCALAPPDATA%\programs\intellij idea community edition\bin',
             'show_icon' => true,
             'force_close' => false,
-            'safe_exam_id' => 1,
+            'safe_exam_id' => $safe_exam->id,
+        ]);
+
+        AllowedApp::create([
+            'title' => 'jetbrains-toolbox',
+            'executable' => 'jetbrains-toolbox.exe',
+            'path' => '%LOCALAPPDATA%\jetbrains\toolbox\bin',
+            'show_icon' => false,
+            'force_close' => true,
+            'safe_exam_id' => $safe_exam->id,
+        ]);
+
+        AllowedApp::create([
+            'title' => 'git-credential-manager',
+            'executable' => 'git-credential-manager.exe',
+            'path' => 'C:\Program Files\Git\mingw64\bin',
+            'show_icon' => false,
+            'force_close' => true,
+            'safe_exam_id' => $safe_exam->id,
+        ]);
+
+        AllowedApp::create([
+            'title' => 'gitkraken',
+            'executable' => 'gitkraken.exe',
+            'path' => '%LOCALAPPDATA%\gitkraken',
+            'show_icon' => true,
+            'force_close' => false,
+            'safe_exam_id' => $safe_exam->id,
         ]);
     }
 }
