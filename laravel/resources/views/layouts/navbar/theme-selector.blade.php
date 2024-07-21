ea859186364aa93822d0cb89658f1657db7c67fc
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2024-07-21 22:38:01 +0200

    Selector de tema mejorado

--- laravel/resources/views/layouts/navbar/theme-selector.blade.php
+++ laravel/resources/views/layouts/navbar/theme-selector.blade.php
@@ -1,43 +1,31 @@
 <li class="nav-item dropdown">
     <button
-        class="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center text-{{ $debug_text_color }} hover-background"
-        id="bd-theme"
-        type="button"
-        aria-expanded="false"
-        data-bs-toggle="dropdown"
-        data-bs-display="static"
+        class="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center text-{{ $debug_text_color }}"
+        id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static"
         aria-label="Toggle theme (auto)">
-        <i class="bi bi-circle-half me-1 theme-icon-active">
-            <use href="#circle-half"></use>
-        </i>
+        <i class="bi me-1 theme-icon-active"></i>
         <span class="d-md-none ms-2" id="bd-theme-text">{{ __('Toggle theme') }}</span>
     </button>
     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="bd-theme-text">
         <li>
-            <button type="button" class="dropdown-item d-flex align-items-center hover-link"
+            <button type="button" class="dropdown-item d-flex align-items-center"
                     data-bs-theme-value="light" aria-pressed="false">
-                <i class="bi bi-circle-half me-2 theme-icon">
-                    <use href="#sun-fill"></use>
-                </i>
-                {{ __('Light') }}
+                <i class="bi bi-sun-fill me-2 theme-icon" data-icon="bi-sun-fill"></i>
+                <span>{{ __('Light') }}</span>
             </button>
         </li>
         <li>
-            <button type="button" class="dropdown-item d-flex align-items-center hover-link"
+            <button type="button" class="dropdown-item d-flex align-items-center"
                     data-bs-theme-value="dark" aria-pressed="false">
-                <i class="bi bi-moon-stars-fill me-2 theme-icon">
-                    <use href="#moon-stars-fill"></use>
-                </i>
-                {{ __('Dark') }}
+                <i class="bi bi-moon-stars-fill me-2 theme-icon" data-icon="bi-moon-stars-fill"></i>
+                <span>{{ __('Dark') }}</span>
             </button>
         </li>
         <li>
-            <button type="button" class="dropdown-item d-flex align-items-center active hover-link"
+            <button type="button" class="dropdown-item d-flex align-items-center active"
                     data-bs-theme-value="auto" aria-pressed="false">
-                <i class="bi bi-circle-half me-2 theme-icon">
-                    <use href="#circle-half"></use>
-                </i>
-                {{ __('Auto') }}
+                <i class="bi bi-circle-half me-2 theme-icon" data-icon="bi-circle-half"></i>
+                <span>{{ __('Auto') }}</span>
             </button>
         </li>
     </ul>
